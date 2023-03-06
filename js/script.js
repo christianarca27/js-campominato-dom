944e53b84b9ccca37c9c4a3b6236178e3a0a57c6
christianarca27 <christian.arca.mail@gmail.com>
2023-03-06 10:41:31 +0100

    refactor: organizzato script in funzioni, cambiati alcuni id e miglioramenti stile

--- js/script.js
+++ js/script.js
@@ -1,40 +1,57 @@
-const levelEl = document.getElementById("level");
-const buttonPlayEl = document.getElementById("btn-play");
+const buttonPlayEl = document.getElementById("btn-new-game");
 
 buttonPlayEl.addEventListener("click", function() {
-    const playTableEl = document.getElementById("play-table");
-    playTableEl.innerHTML = "";
+    newGame();
+});
 
+
+
+
+
+// -------------------------- FUNCTIONS -----------------------------------------
+
+function newGame() {
+    const gameTableEl = document.getElementById("game-table");
+    gameTableEl.innerHTML = "";
+    
+    const levelEl = document.getElementById("level");
     let numSquares;
     if(levelEl.value == "1") {
         numSquares = 100;
-        widthSquare = "calc(100% / 10)";
-        heightSquare = "calc(100% / 10)";
     }
     else if (levelEl.value == "2") {
         numSquares = 81;
-        widthSquare = "calc(100% / 9)";
-        heightSquare = "calc(100% / 9)";
     }
     else {
         numSquares = 49;
-        widthSquare = "calc(100% / 7)";
-        heightSquare = "calc(100% / 7)";
     }
+
+    generateAndShowSquares(gameTableEl, numSquares);
+}
+
+function generateAndShowSquares(gameTableEl, numSquares) {
+    let squareSize = Math.sqrt(numSquares);
 
     for(let i = 1; i <= numSquares; i++) {
 
-        let newEl = document.createElement("div");
-        newEl.classList.add("square");
-        newEl.innerText = i;
-        newEl.style.width = widthSquare;
-        newEl.style.height = heightSquare;
-        playTableEl.append(newEl);
+        const newSquareEl = generateSquare(squareSize);
+        newSquareEl.innerText = i;
+        gameTableEl.append(newSquareEl);
 
-        newEl.addEventListener("click", function() {
+        newSquareEl.addEventListener("click", function() {
             console.log(i);
-            newEl.style.backgroundColor = "lightblue";
+            newSquareEl.style.backgroundColor = "lightblue";
         });
         
     }
-});
+}
+
+function generateSquare(squareSize) {
+    let newEl = document.createElement("div");
+
+    newEl.classList.add("square");
+    newEl.style.width = "calc(100% / " + squareSize + ")";
+    newEl.style.height = "calc(100% / " + squareSize + ")";
+
+    return newEl;
+}
